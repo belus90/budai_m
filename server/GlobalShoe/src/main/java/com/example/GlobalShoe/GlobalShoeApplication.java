7ceb2e4a8ccc8830793833e38a331af163676b65
belus90 <belus90@gmail.com>
2022-08-23 10:46:05 +0100

    server side set up

---
+++ server/GlobalShoe/src/main/java/com/example/GlobalShoe/GlobalShoeApplication.java
@@ -0,0 +1,13 @@
+package com.example.GlobalShoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalShoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalShoeApplication.class, args);
+	}
+
+}
